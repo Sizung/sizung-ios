4d37ca5ad2fc51a72600696d22d30fa62d9fe6c9
Markus Klepp <me@mklepp.com>
2016-06-17 15:40:00 +0200

    Style parsed markdown with correct types

--- Sizung/SizungMarkdownParser.swift
+++ Sizung/SizungMarkdownParser.swift
@@ -10,9 +10,10 @@ import Bumblebee
 
 class SizungMarkdownParser {
   
-  let fontSize: CGFloat = 17
+  let fontSize: CGFloat = 13
   
   var markdown = BumbleBee()
+  var defaultAttr: Dictionary<String, AnyObject> = [:]
   
   init() {
     // italic pattern
@@ -21,21 +22,24 @@ class SizungMarkdownParser {
         return (pattern, nil)
       }
       let replace = pattern[pattern.startIndex.advancedBy(1)...pattern.endIndex.advancedBy(-2)]
-      return (replace,[NSFontAttributeName: UIFont.italicSystemFontOfSize(self.fontSize), NSForegroundColorAttributeName: UIColor.redColor()])
+      return (replace,[NSFontAttributeName: UIFont.italicSystemFontOfSize(self.fontSize)])
     }
     //bold pattern
     markdown.add("**?**", recursive: false) { (pattern: String, text: String, start: Int) -> (String, [NSObject : AnyObject]?) in
       let replace = pattern[pattern.startIndex.advancedBy(2)...pattern.endIndex.advancedBy(-3)]
-      return (replace,[NSFontAttributeName: UIFont.boldSystemFontOfSize(self.fontSize), NSForegroundColorAttributeName: UIColor.blueColor()])
+      return (replace,[NSFontAttributeName: R.font.brandonTextW01Medium(size: self.fontSize)!])
     }
     //mention pattern
     markdown.add("@[?](?)", recursive: false) { (pattern: String, text: String, start: Int) -> (String, [NSObject : AnyObject]?) in
       let replace = pattern[pattern.rangeOfString("[")!.startIndex.advancedBy(1)...pattern.rangeOfString("]")!.endIndex.advancedBy(-2)]
-      return (replace,[NSFontAttributeName: UIFont.boldSystemFontOfSize(self.fontSize), NSForegroundColorAttributeName: UIColor.greenColor()])
+      return (replace,[NSFontAttributeName: R.font.brandonGrotesqueBold(size: self.fontSize)!])
     }
+    
+    // default font
+    defaultAttr = [NSFontAttributeName: R.font.brandonGrotesqueMedium(size: self.fontSize)!]
   }
   
   func parseMarkdown(rawText: String) -> NSAttributedString {
-    return markdown.process(rawText)
+    return markdown.process(rawText, attributes: defaultAttr)
   }
 }
