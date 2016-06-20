013b0adfef8b00def546ffbb896b56e797a4749e
Markus Klepp <me@mklepp.com>
2016-06-20 17:54:43 +0200

    Add unseenobjects infrastructur

---
+++ Sizung/UnseenObjectsResponse.swift
@@ -0,0 +1,22 @@
+//
+//  UnseenObjectsResponse.swift
+//  Sizung
+//
+//  Created by Markus Klepp on 20/06/16.
+//  Copyright © 2016 Sizung. All rights reserved.
+//
+
+import ObjectMapper
+
+class UnseenObjectsResponse: Mappable {
+  
+  var unseenObjects: [UnseenObject]!
+  
+  required init?(_ map: Map) {
+    
+  }
+  
+  func mapping(map: Map) {
+    unseenObjects <- map["data"]
+  }
+}
