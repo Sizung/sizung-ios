f63c242c300c63a980319377b8d3b677f1c5faf0
Markus Klepp <me@mklepp.com>
2016-06-22 09:46:00 +0200

    Reset playground

--- Sizung/MyPlayground.playground/Contents.swift
+++ Sizung/MyPlayground.playground/Contents.swift
@@ -2,11 +2,3 @@
 
 import UIKit
 
-let formatter = NSDateFormatter()
-formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-
-let dateString = "2016-05-04T12:23:46.504Z"
-
-
-formatter.dateFromString(dateString)
