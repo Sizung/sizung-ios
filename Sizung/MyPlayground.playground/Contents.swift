4c8b27de6defd9f189f61ef6f699c7f03b78defe
Markus Klepp <me@mklepp.com>
2016-06-22 10:51:41 +0200

    Cleanup playground

--- Sizung/MyPlayground.playground/Contents.swift
+++ Sizung/MyPlayground.playground/Contents.swift
@@ -1,11 +1,3 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-let currentDate = NSDate
-
-let unsortedArray = [1,3,4,2]
-
-unsortedArray.sort { left, right in
-  return left.comp
-}
