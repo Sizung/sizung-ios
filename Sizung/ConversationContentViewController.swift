59549ee36686db14767d2d390fb7480a210d6072
Markus Klepp <me@mklepp.com>
2016-07-07 09:59:52 +0200

    Add new conversationcontentview

--- Sizung/ConversationContentViewController.swift
+++ Sizung/ConversationContentViewController.swift
@@ -11,8 +11,6 @@ import SwiftKeychainWrapper
 
 class ConversationContentViewController: UIViewController, MainPageViewControllerDelegate {
 
-
-  @IBOutlet weak var titleButton: UIButton!
   @IBOutlet weak var segmentedControl: SizungSegmentedControl!
 
   var mainPageViewController: MainPageViewController!
@@ -21,7 +19,6 @@ class ConversationContentViewController: UIViewController, MainPageViewControlle
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.titleButton.setTitle("@\(conversation.title)", forState: .Normal)
 
     segmentedControl.items = ["PRIORITIES", "CHAT", "ACTIONS"]
     segmentedControl.thumbColors = [Color.TODISCUSS, Color.CHAT, Color.TODO]
@@ -39,28 +36,24 @@ class ConversationContentViewController: UIViewController, MainPageViewControlle
     self.mainPageViewController.setSelectedIndex(selectedIndex)
   }
 
-  @IBAction func close(sender: AnyObject) {
-    dismissViewControllerAnimated(true, completion: nil)
-  }
-
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if R.segue.conversationViewController.embed(segue: segue) != nil {
+    if R.segue.conversationContentViewController.embed(segue: segue) != nil {
       if let mainPageViewController = segue.destinationViewController as? MainPageViewController {
         self.mainPageViewController = mainPageViewController
         self.mainPageViewController.mainPageViewControllerDelegate = self
 
         let agendaItemsTableViewController =
-          R.storyboard.conversations.agendaItemsTableViewController()!
+          R.storyboard.conversation.agendaItemsTableViewController()!
         agendaItemsTableViewController.conversation = self.conversation
 
         self.mainPageViewController.orderedViewControllers.append(agendaItemsTableViewController)
 
-        let timelineTableViewController = R.storyboard.conversations.timelineTableViewController()!
+        let timelineTableViewController = R.storyboard.conversation.timelineTableViewController()!
         timelineTableViewController.timelineParent = self.conversation
         self.mainPageViewController.orderedViewControllers.append(timelineTableViewController)
 
         let deliverablesTableViewController =
-          R.storyboard.conversations.conversationDeliverablesTableViewController()!
+          R.storyboard.conversation.conversationDeliverablesTableViewController()!
         deliverablesTableViewController.conversation = conversation
         self.mainPageViewController.orderedViewControllers.append(deliverablesTableViewController)
       } else {
