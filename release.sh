9b43a1d2c4d53bd73bded3ae9d40b910d2fe89ea
Markus Klepp <me@mklepp.com>
2016-08-25 10:29:47 +0200

    Improve release script

--- release.sh
+++ release.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-# first, build the beta versaion
-xcodebuild clean archive -workspace Sizung.xcworkspace -archivePath "./archives/SizungBeta_$(git rev-parse --abbrev-ref HEAD)" -scheme SizungStaging
+# stop on error
+set -e
 
-# build production
-xcodebuild clean archive -workspace Sizung.xcworkspace -archivePath "./archives/Sizung_$(git rev-parse --abbrev-ref HEAD)" -scheme Sizung
-open "./archives/Sizung_$(git rev-parse --abbrev-ref HEAD)"
+echo "Enter Email of iTunes Connect user, followed by [ENTER]:"
+read connect_email
 
-open archives
+echo "Enter Password of iTunes Connect user, followed by [ENTER] (Will not be shown here):"
+read -s connect_password
+
+# build beta version
+echo "Building beta version - see archives/beta.log"
+xcodebuild clean archive -workspace Sizung.xcworkspace -scheme SizungStaging 1> archives/beta.log
+
+# store version
+store_filename="./archives/Sizung_$(git rev-parse --abbrev-ref HEAD)"
+# build
+echo "Building store version - see archives/store.log"
+xcodebuild clean archive -workspace Sizung.xcworkspace -archivePath $store_filename -scheme Sizung  1> archives/store.log
+# upload
+echo "Uploading store version - see archives/upload.log"
+altool --upload-app -f "$(store_filename).xcarchive" -u $connect_email -p $connect_password 1> archives/upload.log
 
 # clean
-xcodebuild clean -workspace Sizung.xcworkspace -archivePath "./archives/Sizung_$(git rev-parse --abbrev-ref HEAD)" -scheme Sizung
+echo "Cleanup step - see archives/clean.log"
+xcodebuild clean build -workspace Sizung.xcworkspace -scheme Sizung 1> archives/clean.log
+
+# delete logs on success
+rm archives/*.log
